5247e122e8aac2ac933d15ea46020c6a7781f278
Adrien Benete <adrbenete@gmail.com>
2022-03-10 09:15:53 +0900

    utilisation de la librairie carbon

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -4,5 +4,15 @@
 <br>
 
 <h1><a href="">{{ $post->content }}</a></h1>
+<br><br>
+@forelse($post->comments as $comment)
+
+
+
+<p>{{ $comment->content }} | created at {{ $comment->created_at->format('d m Y')}}</p>
+@empty
+
+<span>Aucun commentaire pour ce poste</span>
+@endforelse
 
 @endsection
